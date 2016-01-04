7df817bbc67d1cf3efd722eec1e111268865f2ed
Andrew Ovens <andrew.ovens@replicon.com>
2016-01-04 11:21:31 -0700

    Fix initializer deprecation

--- app/initializers/mixpanel.js
+++ app/initializers/mixpanel.js
@@ -1,10 +1,20 @@
 import MixpanelMixin from '../mixin/tracking_mixin'
 
 export function initialize(container) {
-    var router = container.lookup('router:main');
-    router.on('didTransition', function() {
-      this.trackRouteChange(this.get('url'));
-    });
+    if (container.lookup) {
+      var router = container.lookup('router:main');
+      router.on('didTransition', function() {
+        this.trackRouteChange(this.get('url'));
+      });
+    } else {
+      var routerFactory = container.resolveRegistration('router:main');
+      routerFactory.reopen({
+        didTransition: function didTransition() {
+          this.trackRouteChange(this.get('url'));
+          return this._super.apply(this, arguments);
+        }
+      });
+    }
 }
 
 export default {
